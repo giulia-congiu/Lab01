575abc6628625dee3d04c28488ad070b2c2d8f9a
323346 <giuliauni0004@gmail.com>
2026-03-14 16:20:01 +0100

    completato con soluzione

--- main.py
+++ main.py
@@ -4,21 +4,29 @@ from domanda import Domanda
 from giocatore import Giocatore
 
 f = open("domande.txt", "r").read().splitlines()
-d=[]
+d=[] #creo una lista di oggetti domanda legendo il file
 for ii in range(0, len(f), 7):
+    #step = 7 → vai avanti di 7 in 7: sette solo le righe di 1 domanda
     d.append(Domanda(testo=f[ii], diff=f[ii+1], corretta=f[ii+2], opzioni=f[ii+2:ii+6]))
 
 flag=True
 current_diff = 0
 max_diff = max(d, key=lambda x: x.difficoltà).difficoltà
+# max() trova il massimo di una lista, ma con key gli dici come confrontare gli elementi
 punti=0
 
 
 while(flag):
-    current_d=[]
+    current_d=[] #creo una sottolista di domande con la difficoltà desiderata
     current_d = [x for x in d if int(x.difficoltà)==current_diff]
-    ii = random.randint(0,len(current_d)-1)
-    current_opzioni = current_d[ii].opzioni_random()
+    # La versione "lunga" equivalente:
+    # current_d = []
+    # for x in d:
+    #     if int(x.difficoltà) == current_diff:
+    #         current_d.append(x)
+
+    ii = random.randint(0,len(current_d)-1) # → numero casuale tra 0 e l'ultimo indice della lista
+    current_opzioni = current_d[ii].opzioni_random() #fa lo shuffle delle opzioni della domanda
     print("Livello "+ current_d[ii].difficoltà + ") "+ current_d[ii].testo)
     for jj in range(len(current_opzioni)):
         print(str(jj+1) + '. ' + current_opzioni[jj])
